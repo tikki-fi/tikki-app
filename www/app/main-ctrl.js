df5856506531678512ed92d9b85d148483b56668
Ilmari Kumpula <ilmari@vainu.io>
2018-12-11 20:45:15 +0200

    Finishing main-ctrl.js

--- www/app/main-ctrl.js
+++ www/app/main-ctrl.js
@@ -53,87 +53,82 @@
     activate();
 
     function activate() {
-        $scope.ui = {
-            cityChosen: false
-        };
-        if(angular.isUndefined($rootScope.socialProfile)) {
-            $rootScope.socialProfile = userService.getSocialProfileFromStorage();
+      $scope.ui = {
+        cityChosen: false
+      };
+      if (angular.isUndefined($rootScope.socialProfile)) {
+        $rootScope.socialProfile = userService.getSocialProfileFromStorage();
+      }
+
+      if (angular.isUndefined($rootScope.user) && $rootScope.socialProfile !== null) {
+        $rootScope.user = sharedService.mapSocialProfileData();
+
+        console.log($rootScope.user); // TODO-DEBUG
+      }
+
+      $scope.searchCities = function (searchFilter) {
+        sharedService.searchCities(searchFilter).then(function (matches) {
+          $scope.data = {};
+          $scope.data.cities = matches;
+        });
+      };
+
+      $scope.setUserCity = function (city) {
+        if (angular.isUndefined(city)) {
+          $scope.ui.cityChosen = false;
+        } else {
+          $rootScope.user.city = city;
+          $scope.ui.cityChosen = true;
         }
-
-        if(angular.isUndefined($rootScope.user) && $rootScope.socialProfile !== null) {
-            $rootScope.user = sharedService.mapSocialProfileData();
-
-              console.log($rootScope.user);
-        }
-
-        $scope.searchCities = function (searchFilter) {
-            sharedService.searchCities(searchFilter).then(function (matches) {
-                $scope.data = {};
-                $scope.data.cities = matches;
-            });
-        };
-
-        $scope.setUserCity = function (city) {
-            if(angular.isUndefined(city)) {
-                $scope.ui.cityChosen = false;
-            } else {
-                $rootScope.user.city = city;
-                $scope.ui.cityChosen = true;
-            }
-        }
-
-
+      };
     };
 
     // create new user
     $scope.createUser = function () {
-        userService.getNewUserId().then(function (response) {
-            // set user to rootscope
-            $rootScope.user = {};
-            $rootScope.user.id = response;
-            $state.go('register');
-        });
-
+      userService.getNewUserId().then(function (response) {
+        $rootScope.user = {};
+        $rootScope.user.id = response;
+        $state.go('register');
+      });
     };
 
 
     // toggle genders
     $scope.setGender = function (gender) {
-        if($rootScope.user) {
-            if(gender == 1) {
-                $rootScope.user.gender = "male";
-            }
-            if(gender == 2) {
-                $rootScope.user.gender = "female";
-            }
-        };
+      if ($rootScope.user) {
+        if (gender === 1) {
+          $rootScope.user.gender = "male";
+        }
+        if (gender === 2) {
+          $rootScope.user.gender = "female";
+        }
+      };
     };
 
     $scope.createUserInfo = function (user) {
-        if(!user.id) {
-            user.id = localStorage.getItem('id');
-        }
-        userService.createUserInfo(user).then(function (response) {
-            $ionicHistory.nextViewOptions({
-                disableBack: true
-            });
-            $state.go('profile');
+      if (!user.id) {
+        user.id = localStorage.getItem('id');
+      }
+      userService.createUserInfo(user).then(function (response) { // TODO-DEBUG unused response
+        $ionicHistory.nextViewOptions({
+          disableBack: true
         });
+        $state.go('profile');
+      });
     };
 
     // datepicker for birthdate
-    $scope.openDatePicker = function(){
-        ionicDatePicker.openDatePicker(birthDatePicker);
+    $scope.openDatePicker = function () {
+      ionicDatePicker.openDatePicker(birthDatePicker);
     };
 
     $scope.isSubmitDisabled = function (user) {
-        if(user) {
-            if(user.gender && user.city && user.birthDate && user.firstName && user.lastName) {
-                return false;
-            }
+      if (user) {
+        if (user.gender && user.city && user.birthDate && user.firstName && user.lastName) {
+          return false;
         }
-        return true;
+      }
+      return true;
     };
-};
-
+  };
 })();
